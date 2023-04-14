510d59ba2e412d2a5484f963b5a696df7a8d4766
H3NR1K <mathsilvaesilva@gmail.com>
2023-04-14 11:27:56 -0300

    Novas funcionalidades

--- 14-04/src/index.ts
+++ 14-04/src/index.ts
@@ -4,7 +4,8 @@
 //Instalar Tsx : npm i tsx -D
 //Chamando tudo que foi instalado: npm i
 //Chamar express
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { Produto } from './Models/produto.model';
 
 //Criando um servidor web através do express
 const app = express();
@@ -12,27 +13,31 @@ const app = express();
 //Configurar a aplicação apra receber dados no formato JSON
 app.use(express.json());
 
+let produtos : Produto[] = [];
+
 //Configurando uma requisição com o metodo HTTP GET para a raiz da aplicação
-app.get("/", function(req, res){
+app.get("/", function(req : Request, res : Response) : Response{
     //Gerando uma resposta
-    res.status(200).json({message : "Resposta enviada em JSON"});
+    return res.status(200).json({message : "Listagem de produtos", listaProd : produtos});
 });
 
 //Receber informação pela URL
-app.get("/:categoria/:produto", (req, res) => {
-    // console.log(req.params.produto, req.params.categoria);
-    // const produto = req.params.produto
-    // const categoria = req.params.categoria
-    // console.log(produto, categoria)
-
-    const { produto, categoria } = req.params;
-    res.status(200).json({ categoria, produto });
+app.get("/:produto/:preco", (req : Request, res : Response) : Response => {
+    const { preco, produto } = req.params;
+    return res.status(200).json({ produto, preco });
 });
 
 //Enviar informações pelo corpo da aplicação
-app.post("/", (req, res) => {
-    res.status(201).json(
-        { mensage : "Categoria cadastrada!" , categoria : req.body}
+app.post("/", (req : Request, res : Response) : Response => {
+    let produto : Produto = new  Produto();
+
+    produto.nome = req.body.nome;
+    produto.preco = req.body.preco;
+    
+    produtos.push(produto);
+
+    return res.status(201).json(
+        { mensage : "Produto cadastrado!" , newProd : produto}
     );
 });
 
