e96a9c69afce01cf18a78e5ba90f21478a631017
H3NR1K <mathsilvaesilva@gmail.com>
2023-04-28 11:22:25 -0300

    Melhora do index

--- 14-04/src/index.ts
+++ 14-04/src/index.ts
@@ -4,42 +4,16 @@
 //Instalar Tsx : npm i tsx -D
 //Chamando tudo que foi instalado: npm i
 //Chamar express
-import express, { Request, Response } from 'express';
-import { Produto } from './Models/produto.model';
+import express from 'express';
+import { router } from './Config/routes';
+
 
 //Criando um servidor web através do express
 const app = express();
 
 //Configurar a aplicação apra receber dados no formato JSON
 app.use(express.json());
-
-let produtos : Produto[] = [];
-
-//Configurando uma requisição com o metodo HTTP GET para a raiz da aplicação
-app.get("/", function(req : Request, res : Response) : Response{
-    //Gerando uma resposta
-    return res.status(200).json({message : "Listagem de produtos", listaProd : produtos});
-});
-
-//Receber informação pela URL
-app.get("/:produto/:preco", (req : Request, res : Response) : Response => {
-    const { preco, produto } = req.params;
-    return res.status(200).json({ produto, preco });
-});
-
-//Enviar informações pelo corpo da aplicação
-app.post("/", (req : Request, res : Response) : Response => {
-    let produto : Produto = new  Produto();
-
-    produto.nome = req.body.nome;
-    produto.preco = req.body.preco;
-    
-    produtos.push(produto);
-
-    return res.status(201).json(
-        { mensage : "Produto cadastrado!" , newProd : produto}
-    );
-});
+app.use(router);
 
 //O comando listen roda a aplicação
 app.listen(3000, function(){
